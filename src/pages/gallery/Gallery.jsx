1f7c5acceaec5a6a0dd85c879de2503ed2045533
wessouzza <wes-souza@hotmail.com>
2025-01-02 21:07:23 -0300

    Update: Gallery component

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,61 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Card from "../../components/card/Card";
 import PictureModal from "../../components/pictureModal/PictureModal";
 import { SearchProvider, useSearch } from "../../context/SearchContext";
 import { FaAnglesRight } from "react-icons/fa6";
 import { FaAnglesLeft } from "react-icons/fa6";
+import { useFetchArt } from "../../hooks/useFetchArt";
+
 
 const Gallery = () => {
   const [modal, setModal] = useState(false);
   const [selected, setSelected] = useState(null);
-  const [data, setData] = useState([]);
   const [contentLimit, setContentLimit] = useState(7);
-  const [page, setPage] = useState(1);
-  const { searchTerm, setSearchTerm } = useSearch();
 
-  const prevPage = () => {
-    if (page <= 1) return;
-    setPage(page - 1);
-  };
-
-  const nextPage = () => {
-    setPage(page + 1);
-  };
-
-  const fetchPagination = async () => {
-    const response = await fetch(
-      `https://api.artic.edu/api/v1/artworks?page=${page}&limit=7&fields=id,thumbnail,short_description,date_display,title,artist_display,width,height,alt_text,image_id`
-    );
-    const result = await response.json();
-    setData(result.data);
-  };
-
-  useEffect(() => {
-    fetchPagination();
-  }, [page]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://api.artic.edu/api/v1/artworks?fields=id,thumbnail,short_description,date_display,title,artist_display,date_display,alt_text,image_id,width,height"
-      );
-      const result = await response.json();
-      setData(result.data);
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const searchData = async () => {
-      const response = await fetch(
-        `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&fields=id,thumbnail,alt_text,image_id,title,date_display,artist_display,alt_text,short_description,width,height`
-      );
-      const result = await response.json();
-      setData(result.data);
-    };
-    searchData();
-  }, [searchTerm]);
+  const {art, state, searchTerm, isImgLoading, dispatch} = useFetchArt();
 
   const openModal = (card) => {
     setSelected(card);
@@ -66,6 +24,7 @@ const Gallery = () => {
     setModal(false);
     setSelected(null);
   };
+  
 
   return (
     <>
@@ -75,9 +34,8 @@ const Gallery = () => {
         </h1>
         <section className="card-container">
           <div className="card-grid">
-            {data.length > 0 ? (
-              data
-                .slice(1, contentLimit)
+            {art.length > 0 ? (
+                art.slice(1, contentLimit)
                 .map((item) => (
                   <Card
                     key={item.id}
@@ -85,6 +43,7 @@ const Gallery = () => {
                     title={item.title}
                     author={item.artist_display}
                     alt_text={item.alt_text}
+                    isImgLoading={isImgLoading}
                     onClick={() => openModal(item)}
                   />
                 ))
@@ -107,11 +66,11 @@ const Gallery = () => {
           )}
         </section>
         <div className="pagination-control">
-          <button onClick={() => prevPage()}>
+          <button onClick={() => dispatch({type: 'prevPage'})}>
             <FaAnglesLeft />
           </button>
-          <p>{page}</p>
-          <button onClick={() => nextPage()}>
+          <p>{state.page}</p>
+          <button onClick={() => dispatch({type: 'nextPage'})}>
             <FaAnglesRight />
           </button>
         </div>
